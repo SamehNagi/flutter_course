f929deddd05056affacd25480a0a5336917a71c4
Sameh Nagi <eng.same7.la@gmail.com>
2022-11-01 14:05:09 +0100

    Logout view

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -5,6 +5,9 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
+// Only import a specific part of the package developer and give the package an alias
+// and then use it as devtools.log
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,20 +39,20 @@ class HomePage extends StatelessWidget {
         // that object is actually the result of your future which is the Firebase app.
         // A future has a start point, a line where it prrocesses its info, and an end point.
         // The snapshot is your your way of getting the results of your future,
-        //-whether it is started, is it processing, is it done, or did it failed.
+        // whether it is started, is it processing, is it done, or did it failed.
         switch (snapshot.connectionState) {
           case ConnectionState.done:
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print('Email is verified');
+                // print('Email is verified');
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
             } else {
               return const LoginView();
             }
-            return const Text('Done');
           // print(user?.emailVerified);
           // if (user?.emailVerified ?? false) {
           //   return const Text('Done');
@@ -68,4 +71,85 @@ class HomePage extends StatelessWidget {
       },
     );
   }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main UI"),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              // print(value);
+              // devtools.log(value.toString());
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  // devtools.log(ShouldLogout.toString());
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/',
+                      (route) => false,
+                    );
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              // Here the itemBuilder is returning a list of PopupMenuEntery and the PopupMenuItem is of
+              // type PopupMenuEntery so, we do the following: return [ add here the PopupMenuItem ].
+              return [
+                const PopupMenuItem<MenuAction>(
+                  // The value is what the programmer sees and the child is what the user sees
+                  value: MenuAction.logout,
+                  child: Text('Log out'),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text("Hello world"),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel')),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out')),
+        ],
+      );
+    },
+  ).then(
+      (value) => value ?? false); // What we did here is because the showDialog
+  // is returning a future of a possible value while our showLogOutDialog is returning a
+  // future of a bool. The user might be able to dismiss the dialog without actually
+  // selecting Cancel or Logout causing the showdialog to return null. So, we added
+  // then function to return the value if it is present of return false otherwise.
 }
