2cadb131aec64cf386c40564a6e624537f2a9594
Sameh Nagi <eng.same7.la@gmail.com>
2022-11-15 23:13:28 +0100

    Error Handling in Register View, Next Screen After Registration

--- mynotes/lib/views/register_view.dart
+++ mynotes/lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 // Only import a specific part of the package developer and give the package an alias
 // and then use it as devtools.log
 import 'dart:developer' as devtools show log;
@@ -65,29 +66,64 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredentials =
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                // final userCredentials =
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
                   email: email,
                   password: password,
                 );
-                devtools.log(userCredentials.toString());
+                // devtools.log(userCredentials.toString());
+                // The desired effect here is that our register page is gonna stay right here,
+                // and then we are gonna push the verifyEmailRoute on top of it, if the users
+                // in the verifyEmailRoute realize that they have done something wrong with
+                // the email address, they can just press the back button on the top bar and
+                // go 0back to the register page.
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                Navigator.of(context).pushNamed(verifyEmailRoute);
               } on FirebaseAuthException catch (e) {
                 // print(e.code);
                 if (e.code == 'weak-password') {
-                  devtools.log('Weak password');
+                  // devtools.log('Weak password');
+                  await showErrorDialog(
+                    context,
+                    'Weak password',
+                  );
                 }
                 // else {
                 //   print(e);
                 // }
                 else if (e.code == 'email-already-in-use') {
-                  devtools.log('Email is already in use');
+                  // devtools.log('Email is already in use');
+                  await showErrorDialog(
+                    context,
+                    'Email is already in use',
+                  );
                 }
                 // else {
                 //   print(e.code);
                 // }
                 else if (e.code == 'invalid-email') {
-                  devtools.log('Invalid email entered');
+                  // devtools.log('Invalid email entered');
+                  await showErrorDialog(
+                    context,
+                    'This is an invalid email address',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
                 }
+                // Handling other errors that might arise
+                // This is a generic catch block meaning that if the exception that occurs in the try
+                // statement is not a Firebase athentication exception, then in this catch block.
+              } catch (e) {
+                // Here any other exception is an object not known to me and it is up to you to
+                // display that error.
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text('Register'),
--- mynotes/lib/views/verify_email_view.dart
+++ mynotes/lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -34,13 +35,28 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       ),
       body: Column(
         children: [
-          const Text('Please verify your email address:'),
+          const Text(
+              "We've sent you an email verification, please open it to verify your account."),
+          const Text(
+              "If you haven't received a verification email yet, press the button below"),
           TextButton(
             onPressed: () async {
               final user = FirebaseAuth.instance.currentUser;
               await user?.sendEmailVerification();
             },
             child: const Text('Send email verification'),
+          ),
+          // We need a restart button to end the verify email view and sign the user out so
+          // that we don't get stuck with verifyEmailView.
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (route) => false,
+              );
+            },
+            child: const Text('Restart'),
           ),
         ],
       ),
